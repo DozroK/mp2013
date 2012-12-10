1a452a4a2817dc2ab6f79678555c8f3674d4a489
Ka <kcassam@gmail.com>
2012-12-10 10:58:51 +0100

    tools

--- tools/createdatabase.php
+++ tools/createdatabase.php
@@ -2,13 +2,13 @@
 $host="127.0.0.1"; 
 $root="root"; 
 $root_password="root"; 
-$db="mp"; 
+$db="api"; 
 
 try {
-    $dbh = new PDO("mysql:host=$host", $root, $root_password);
-
-    $dbh->exec("CREATE DATABASE `$db`;") 
-    or die(print_r($db->errorInfo(), true));
+    $dbh = new PDO("mysql:host=$host;dbname=$db", $root, $root_password);
+    $query = $dbh->query("") ;
+    
+    var_dump($query);
 
 } catch (PDOException $e) {
     die("DB ERROR: ". $e->getMessage());
