132b9f523009161535cd60e08234ff136a9703e2
Ka <kcassam@gmail.com>
2012-12-24 17:37:02 +0100

    RDF - Add the UTF8 bom header in the document
    https://github.com/DozroK/mp2013/issues/21

--- views/onlyIdRdf.php
+++ views/onlyIdRdf.php
@@ -1,4 +1,4 @@
-<?php echo "<?xml version='1.0'?>" ?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 <rdf___RDF 
     xmlns___rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
     xmlns___rdfs="http://www.w3.org/2000/01/rdf-schema#"
